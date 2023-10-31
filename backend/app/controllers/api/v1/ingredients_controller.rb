6d3e00b097a28c05b6e1e6a79e2b40dd7b79285d
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-10-31 23:14:42 +0900

    backend/ transactionを追加

--- backend/app/controllers/api/v1/ingredients_controller.rb
+++ backend/app/controllers/api/v1/ingredients_controller.rb
@@ -8,28 +8,33 @@ class Api::V1::IngredientsController < ApplicationController
 
     def update
         new_ingredients = params[:ingredients]
-        new_ingredient_ids = new_ingredients.map { |item| item['id'] }.compact
 
-        @recipe.ingredients.where.not(id: new_ingredient_ids).destroy_all
+        ActiveRecord::Base.transaction do
+            new_ingredient_ids = new_ingredients.map { |item| item['id'] }.compact
+            
+            @recipe.ingredients.where.not(id: new_ingredient_ids).each do |ingredient|
+                ingredient.destroy!
+            end
 
-        new_ingredients.each do |item|
-            if item['id'].blank?
-                ingredient = Ingredient.new(name: item['name'], amount: item['amount'])
-                ingredient.recipe_id = @recipe.id
-                if not ingredient.save
-                    render json: { message: "保存できませんでした" , status: :internal_server_error}
-                    return
-                end
-            else
-                ingredient = Ingredient.find(item['id'])
-                if not ingredient.update(name: item['name'], amount: item['amount'])
-                    render json: { message: "保存できませんでした" , status: :internal_server_error}
-                    return
+            new_ingredients.each do |item|
+                if item['id'].blank?
+                    ingredient = Ingredient.new(name: item['name'], amount: item['amount'])
+                    ingredient.recipe_id = @recipe.id
+                    if not ingredient.save!
+                        render json: { message: "保存できませんでした" , status: :internal_server_error}
+                        return
+                    end
+                else
+                    ingredient = Ingredient.find(item['id'])
+                    if not ingredient.update!(name: item['name'], amount: item['amount'])
+                        render json: { message: "保存できませんでした" , status: :internal_server_error}
+                        return
+                    end
                 end
             end
+            
+            render json: {status: 200, ingredients: @recipe.ingredients}
         end
-        
-        render json: {status: 200, ingredients: @recipe.ingredients}
     end
 
     private
--- backend/app/controllers/api/v1/steps_controller.rb
+++ backend/app/controllers/api/v1/steps_controller.rb
@@ -12,23 +12,25 @@ class Api::V1::StepsController < ApplicationController
         else
             new_steps = params[:steps]
 
-        new_steps.each do |item|
-            step = @recipe.steps.find_or_initialize_by(order: item['order'])
-            step.description = item['description']
-            if not step.save
-                render json: { message: "保存できませんでした" , status: 500}
+            ActiveRecord::Base.transaction do
+                new_steps.each do |item|
+                    step = @recipe.steps.find_or_initialize_by(order: item['order'])
+                    step.description = item['description']
+                    if not step.save!
+                        render json: { message: "保存できませんでした" , status: 500}
+                    end
+                end
+                
+                max_order = new_steps[new_steps.length-1]['order']
+                deleted_steps = @recipe.steps.where("`order` > ?", max_order)
+                if deleted_steps
+                    deleted_steps.each do |step|
+                        step.destroy!
+                    end
+                end
+            
+                render json: {status: 200, steps: @recipe.steps}
             end
-        end
-        
-        max_order = new_steps[new_steps.length-1]['order']
-        print(max_order)
-
-        deleted_recipe = @recipe.steps.where("`order` > ?", max_order)
-        if deleted_recipe
-            deleted_recipe.destroy_all
-        end
-        
-        render json: {status: 200, steps: @recipe.steps}
         end
     end
 
