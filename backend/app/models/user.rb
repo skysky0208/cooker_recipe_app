541a8130a064bda4dd56c0ed43cbd3e7740e29a9
Natsumi Sakamoto <55139437+skysky0208@users.noreply.github.com>
2023-11-15 13:38:01 +0900

    1回目のデプロイ

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
   has_many :recipes, dependent: :destroy
